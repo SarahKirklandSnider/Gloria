b4936b9ad881750c7a49ddc750eec82fdb9b3e07
Louise Lessel <louiselessel@gmail.com>
2021-02-14 12:05:04 -0500

    colors correct, only mammoth

--- docs/script.js
+++ docs/script.js
@@ -292,7 +292,7 @@ void main() {
     }
 
     
-    shade = shade * (soundFade + 0.1) * 5.;
+    shade = shade * (soundFade) * 5.;
     
 
     vec4 col = vec4(vec3(c)*tc.rgb * shade + w, 1.);
@@ -379,7 +379,7 @@ class App {
     this.iTime = 0.0;
     this.fader = 0.0;
     this.fadeTime = 0.002;
-    this.timing = 51000.0; // song is 6 mins, each animal gets 51 seconds
+    this.timing = 5000.;//51000.0; // song is 6 mins, each animal gets 51 seconds
 
 
     this.inputIMAGE = this.loader.load('textures/butterfly.png');
@@ -540,7 +540,8 @@ class App {
       this.bufferB.uniforms['soundTriggered'].value = this.soundTriggered;
 
       this.bufferB.uniforms['fader'].value = this.fader;
-
+      this.bufferB.uniforms['iChannel1'].value = this.inputIMAGE5;
+      /*
       // fader starts at 0.0 inputting iChannel 1
       if (iTime > this.timing * 1. && iTime < this.timing * 2.) {
         if (this.fader < 1.0) {
@@ -590,6 +591,7 @@ class App {
           //console.log("6");
         }
       }
+      */
 
       this.bufferB.uniforms['iFrame'].value = this.counter++;
       this.bufferB.uniforms['iChannel0'].value = this.targetA.readBuffer.texture;
