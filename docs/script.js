057b7f83eda28cd5dadf72d1d0ea16e38192cc1c
Louise Lessel <louiselessel@gmail.com>
2021-02-16 22:23:28 -0500

    updated warning text

--- docs/script.js
+++ docs/script.js
@@ -952,7 +952,7 @@ function isMobile(){
   // credit to Timothy Huang for this regex test: 
   // https://dev.to/timhuang/a-simple-way-to-detect-if-browser-is-on-a-mobile-device-with-javascript-44j3
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    alert('play!');  
+    alert('This experience may not play on your device. Please visit on a computer.');  
     return true
  }
  else{
